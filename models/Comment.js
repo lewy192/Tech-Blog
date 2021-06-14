94a8e23f3d1a3302643e8852b8a4f8f825e31895
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-15 08:04:21 +1000

    add:comment Model

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,38 @@
 const { Model, DataTypes } = require("sequelize");
 
+const User = require("./User");
+const Post = require("./Post");
+
 class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: { model: User, key: "id" },
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: { model: Post, key: "id" },
+        },
+    },
+    {
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        freezeTableName: true,
+    }
+);
