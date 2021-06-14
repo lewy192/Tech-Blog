0fe080c4f06cbb79eae618104ac0a556d3196bdb
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-15 07:46:19 +1000

    add: User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,37 @@
 const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING(24),
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(66),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(320),
+            allowNull: false,
+        },
+        firstName: { type: DataTypes.STRING(56), allowNull: false },
+        lastName: { type: DataTypes.STRING(56), allowNull: false },
+    },
+    {
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        freezeTableName: true,
+    }
+);
+
+module.exports = User;
