c38b11690231515b81e1301fef62029b6766eaa6
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-15 07:20:27 +1000

    Post model completed

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,33 @@
 const { Model, DataTypes } = require("sequelize");
+const { User } = require("./User");
 
 class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        postTitle: {
+            type: DataTypes.STRING(52),
+            allowNull: false,
+        },
+        postContent: { type: DataTypes.TEXT, allowNull: false },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: { model: User, key: "id" },
+        },
+    },
+    {
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        freezeTableName: true,
+    }
+);
+
+module.exports = Post;
