0d525e9cf81a5c76a27b84486045679155793850
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-15 13:18:32 +1000

    add: signup route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,30 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const errLoggger = require("../utils/errlog");
 const User = require("../../models/User");
+const { log } = require("handlebars/runtime");
 require("dotenv").config();
 
-router.post("/login", (req, res) => {});
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userLogin = await User.findOne({
+            where: { usernamme: username },
+        });
+        const { password: userPassword } = userLogin;
+        if (!userLogin.checkPassword(userPassword)) {
+            correctDetails = false;
+            res.render("login", { correctDetails });
+        }
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            loggedIn = true;
+            res.render("homepage", { loggedIn });
+        });
+    } catch (err) {
+        errLoggger.errorLogging(err);
+        res.status(500);
+    }
+});
 
 router.post("signup", async (req, res) => {
     try {
@@ -28,6 +49,7 @@ router.post("signup", async (req, res) => {
         );
         req.session.save(() => {
             req.session.loggedIn = true;
+            loggedIn = true;
             res.status(200).render("homepage", { loggedIn });
         });
     } catch (err) {
