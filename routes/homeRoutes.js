09100f458f4c6337cae5499c9d87995f6ad059db
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-15 11:11:23 +1000

    add: homepage, signup and login routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,13 +4,41 @@ const errLoggger = require("../utils/errlog");
 
 router.get("/", async (req, res) => {
     try {
-        if (req.session.logged_in) {
+        if (req.session.loggedIn) {
             res.render("homepage", { logged_in: true });
         }
         res.status(200);
         res.render("homepage", { logged_in: false });
     } catch (err) {
         errLoggger.errorLogging(err);
-        res.status(500);
+        res.status(500).send("whoops");
+    }
+});
+
+router.get("/login", (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.status(200);
+            res.render("homepage", { logged_in: loggedIn });
+        }
+        res.status(200);
+        res.render("login");
+    } catch (err) {
+        errLoggger.errorLogging(err);
+        res.status(500).send("whoops");
+    }
+});
+
+router.get("/signup", (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.status(200);
+            res.render("homepage", { logged_in: loggedIn });
+        }
+        res.status(200);
+        res.render("signup");
+    } catch (err) {
+        errLoggger.errorLogging(err);
+        res.status(500).send("whoops");
     }
 });
