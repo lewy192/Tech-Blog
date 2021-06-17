619a128f44e61207522c88d07ddc9c1b2fbe48c6
lewy192 <lewis.james.hill@outlook.com>
2021-06-17 16:44:31 +1000

    added: user seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,11 @@
 const postData = require("./postSeed.json");
+const userData = require("./userSeed.json");
 const { User, Comment, Post } = require("../models");
+const sequelize = require("../config/connection");
 
 seedAllModels = async () => {
+    await sequelize.sync({ force: false });
+    // const user = await User.bulkCreate(userData);
     const posts = await Post.bulkCreate(postData);
 };
 
