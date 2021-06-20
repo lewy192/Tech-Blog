cf1b52a748462fb65397fc9e495a925436245623
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-20 18:32:16 +1000

    add: create route for new posts

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -7,12 +7,14 @@ const errorMessage500 = "Whoops status 500. Try again soon";
 
 router.post("/create", async (req, res) => {
     try {
+        console.log(req.body);
         const { userId } = req.session;
         const { postTitle, postContent } = req.body;
         const newPost = await Post.create({ postTitle, postContent, userId });
-        res.render("dashboard", { userId });
+        res.redirect("/dashboard");
     } catch (err) {
         console.log(err);
+        res.status(500).send(errorMessage500);
     }
 });
 module.exports = router;
